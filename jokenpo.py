2fae6e99a79bc5a817eac54fdcecec13c745f5ae
errcc <eulerrc86@gmail.com>
2021-08-19 20:46:21 -0300

    Adicionados comentários no arquivo

--- jokenpo.py
+++ jokenpo.py
@@ -1,5 +1,6 @@
 import random
 
+#Função que traduz o número associado à CPU a um dos componentes do jogo
 def escolha_cpu(x):
     if x == 1:
         return "Pedra"
@@ -7,7 +8,8 @@ def escolha_cpu(x):
         return "Papel"
     else:
         return "Tesoura"
-    
+
+#Função que compara as escolhas do jogador e da CPU
 def compara(jogador,cpu):
     if jogador == 1:
         if cpu == 3:
@@ -20,18 +22,18 @@ def compara(jogador,cpu):
             return True
     return False
 
-while x != 
+#while x != 
 print("Digite o número correspondente a sua opção:")
 print("1 - Pedra")
 print("2 - Papel")
 print("3 - Tesoura")
-choice_user = input()
-choice_cpu = random.randint(1,3)
-print(choice_cpu)
+choice_user = input() #captura a resposta do usuário
+choice_cpu = random.randint(1,3) #gera a escolha da CPU
+#print(choice_cpu)
 print("O computador escolheu ",escolha_cpu(choice_cpu))
-if int(choice_user) == choice_cpu:
+if int(choice_user) == choice_cpu: #Mostra se a partida ficar empatada
     print("Empate")
-elif compara(int(choice_user),choice_cpu) == True:
+elif compara(int(choice_user),choice_cpu) == True: #int(choice_user) supr importante, pq input() captura como char
     print("Você venceu!")
 else:
     print("Você perdeu")
